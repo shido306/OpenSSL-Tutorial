7ebb15906c621d78e572c8da9ced9e22d6eae047
shido <shido2910@gmail.com>
2026-06-25 11:48:49 +0900

    ヘルプオプションの追加

---
+++ 02_asymmetric/src/CommandLine.cpp
@@ -0,0 +1,36 @@
+//************************************************************************* 
+//
+// RSA 公開鍵暗号方式
+//
+// コマンドライン引数の処理 ソースファイル
+//
+// ************************************************************************/ 
+
+////////////////////////////
+// インクルード
+////////////////////////////
+#include <iostream>
+#include "../include/CommandLine.hpp"
+
+////////////////////////////
+// 関数定義
+////////////////////////////
+
+// ヘルプ表示
+void usage(
+    const std::filesystem::path& program_path
+){
+    const auto program_name = program_path.filename().generic_string();
+
+    std::cout
+        << "OpenSSLライブラリのRSA公開鍵暗号方式の使用方法のサンプル"        << std::endl
+        << "Usage:"                                       << std::endl
+        << "  " << program_name << " help"                << std::endl
+        << "  " << program_name << " -h"                  << std::endl
+        << "Arguments:"                                   << std::endl
+        << "  help"                                       << std::endl
+        << "    ヘルプを表示する"                         << std::endl
+        << "Options:"                                     << std::endl
+        << "  -h"                                         << std::endl
+        << "    ヘルプを表示する"                         << std::endl;
+}
--- 02_asymmetric/src/main.cpp
+++ 02_asymmetric/src/main.cpp
@@ -2,9 +2,45 @@
 //
 // RSA 公開鍵暗号方式
 //
+// メイン関数 ソースファイル
+//
 // ************************************************************************/ 
 
+////////////////////////////
+// インクルード
+////////////////////////////
+#include <iostream>
+#include <unistd.h>
+#include "../include/CommandLine.hpp"
+
+////////////////////////////
+// 関数定義
+////////////////////////////
+
+// メイン関数
 int main (int argc, char* argv[]){
+    const auto program_path = argv[0];
+    int opt;
+
+    while((opt = getopt(argc, argv, "h")) != -1 ){
+        switch( opt ){
+            case 'h':   // ヘルプオプション
+                usage(program_path);
+                return 0;
+            default:
+                return 1;
+        }
+    }    
+
+    if( argc - optind < 1 ){
+        std::cerr << "no subcommand" << std::endl;
+        return 1;
+    }
+        auto subcommand = std::string(argv[optind]);
+    if( subcommand == "help" ){
+        usage(program_path);
+        return 0;
+    }
 
     return 0;
 }
